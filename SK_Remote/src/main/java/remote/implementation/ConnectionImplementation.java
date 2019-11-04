9b91cce45b060a88b7e80db106b9eda5a0540d6c
Aleksa <trajko1998@gmail.com>
2019-11-04 20:36:33 +0100

    Remote Search, Delete, mkDir

--- SK_Remote/src/main/java/remote/implementation/ConnectionImplementation.java
+++ SK_Remote/src/main/java/remote/implementation/ConnectionImplementation.java
@@ -8,6 +8,7 @@ import com.dropbox.core.v2.files.*;
 import de.vandermeer.asciitable.AsciiTable;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ConnectionImplementation implements Connection {
@@ -16,6 +17,8 @@ public class ConnectionImplementation implements Connection {
     private String currentUser;
     private String password;
     private DbxClientV2 client;
+    private UserPrivilege currentPrivilege;
+
 
     public ConnectionImplementation(String path, String currentUser, String password) {
         this.path = path;
@@ -54,24 +57,69 @@ public class ConnectionImplementation implements Connection {
 
     }
 
-    public boolean mkDir(String[] paths) {
+    public boolean mkDir(String[] arguments) {
+        for (String dirName : arguments) {
+            List<String> strings = new ArrayList<String>();
+            if (dirName.contains("{") && dirName.contains("}")) {
+                String name = dirName.substring(0, dirName.indexOf("{"));
+                String pom = dirName.substring(dirName.indexOf("{") + 1, dirName.indexOf("}"));
+                String[] fromTo = pom.split("-");
+                int from = Integer.parseInt(fromTo[0]);
+                int to = Integer.parseInt(fromTo[1]);
+                for (int i = from; i <= to; i++) {
+                    strings.add(name + i);
+                }
+                try {
+                    client.files().createFolderBatchBuilder(strings).start();
+                } catch (DbxException e) {
+                    System.out.println("There was an error.");
+                    return false;
+                } catch (IllegalArgumentException e2) {
+                    System.out.println("You are probably missing /");
+                    return false;
+                }
+            } else {
+                try {
+                    CreateFolderResult folder = client.files().createFolderV2(dirName, true);
+                    if (folder.getMetadata().getPathLower().equals(path + dirName)) {
+                        System.out.println("Done!");
+                    }
+                } catch (DbxException e) {
+                    System.out.println("Something went wrong.");
+                    return false;
+                } catch (IllegalArgumentException e2) {
+                    System.out.println("You are probably missing /");
+                    return false;
+                }
+            }
+        }
         return true;
     }
 
     public void mkFile(String path) {
-
     }
 
     public void deleteItem(String path) {
-
+        try {
+            DeleteResult deleteResult = client.files().deleteV2(path);
+            if (path.equals(deleteResult.getMetadata().getPathLower())) {
+                System.out.println("Done!");
+            }
+            System.out.println(deleteResult.getMetadata().getPathLower());
+        } catch (DbxException e) {
+            System.out.println("Something went wrong.");
+        }
     }
 
+
     public boolean isLoggedIn() {
-        return true;
+        if (currentUser != null) return true;
+        return false;
     }
 
     public boolean isAdmin() {
-        return false;
+        //  if (currentPrivilege.equals(UserPrivilege.ADMIN)) return true;
+        return true;
     }
 
     public boolean isBlacklisted(String extension) {
@@ -87,23 +135,22 @@ public class ConnectionImplementation implements Connection {
     }
 
     public void Search(String fileName) {
-
-    }
-
-    public void lsDir(String path, boolean subdirectories, boolean isDir) {
-        /*
+        Long max = 100L;
+        Long stat = 0L;
+        SearchResult sresult;
+        SearchMode mode = SearchMode.FILENAME;
         try {
-
-            JSONObject jsonObject = new JSONObject(client.files().listFolder(""));
-            JSONArray jsonArray = jsonObject.getJSONArray("entries");
-            for(int i=0;i<jsonArray.length();i++){
-                JSONObject file = jsonArray.getJSONObject(i);
-                System.out.println(file.getString("name"));
+            sresult = client.files().searchBuilder("", fileName).withMode(mode).withMaxResults(max).withStart(stat).start();
+            List<SearchMatch> res = sresult.getMatches();
+            for (int i = 0; i < res.size(); i++) {
+                System.out.println(res.get(i).getMetadata().getPathLower());
             }
         } catch (DbxException e) {
             System.out.println("Something went wrong ¯\\_(ツ)_/¯ pls try again");
         }
-        */
+    }
+
+    public void lsDir(String path, boolean subdirectories, boolean isDir) {
         ListFolderResult result = null;
 //        SearchBuilder srcb;
 //        SearchMode mode = SearchMode.FILENAME;
